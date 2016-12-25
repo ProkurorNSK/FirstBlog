eeb7c1e116809e48eafb518cc3916217f94479f8
ProkurorNSK <proka@ngs.ru>
2016-12-26 01:59:08 +0700

    Lesson 6

--- models/articles.php
+++ models/articles.php
@@ -1,10 +1,16 @@
 <?php
     function articles_all(){
+        $art1 = ["id" => 1, "title" => "Title1", "date" => "2015_01_01", "content" => "Content1"];
+        $art2 = ["id" => 2, "title" => "Title2", "date" => "2016_01_01", "content" => "Content2"];
         
+        $arr[0] = $art1;
+        $arr[1] = $art2;
+        
+        return $arr;
     }
 
     function articles_get($id){
-        
+        return ["id" => 1, "title" => "Title1", "date" => "2015_01_01", "content" => "Content1"];
     }
 
     function articles_new($title, $date, $content){
